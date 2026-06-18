d246ede9a8ad220987b811ae70d78b0d6b3e96e8
Potnuri Bharat Teja <bharat@chelsio.com>
2026-06-18 11:19:30 +0530

    cxgb4: add T7 hardware definitions and firmware interfaces
    Add the initial T7-specific hardware definitions required by the cxgb4
    driver. This patch updates chip identification, PCI device IDs,
    register definitions, TCB layout definitions, firmware API interfaces,
    firmware version information, message formats, and related constants to
    provide the foundational infrastructure for upcoming T7 support.
    
    No functional changes are introduced by this patch. It only extends the
    existing hardware and firmware definition headers for T7-specific
    driver functionality.
    
    Signed-off-by: Potnuri Bharat Teja <bharat@chelsio.com>

--- drivers/net/ethernet/chelsio/cxgb4/t4_values.h
+++ drivers/net/ethernet/chelsio/cxgb4/t4_values.h
@@ -125,12 +125,20 @@
 /* PCI-E definitions */
 #define WINDOW_SHIFT_X		10
 #define PCIEOFST_SHIFT_X	10
+#define T7_MEMOFST_SHIFT_X	4
 
 /* TP_VLAN_PRI_MAP controls which subset of fields will be present in the
  * Compressed Filter Tuple for LE filters.  Each bit set in TP_VLAN_PRI_MAP
  * selects for a particular field being present.  These fields, when present
  * in the Compressed Filter Tuple, have the following widths in bits.
  */
+#define FT_FIRST_S                      FCOE_S
+#define FT_LAST_S                       FRAGMENTATION_S
+
+#define T7_FT_FIRST_S                   IPSECIDX_S
+#define T7_FT_LAST_S                    TCPFLAGS_S
+
+#define FT_IPSECIDX_W                   12
 #define FT_FCOE_W                       1
 #define FT_PORT_W                       3
 #define FT_VNIC_ID_W                    17
@@ -141,6 +149,9 @@
 #define FT_MACMATCH_W                   9
 #define FT_MPSHITTYPE_W                 3
 #define FT_FRAGMENTATION_W              1
+#define FT_ROCE_W			1
+#define FT_SYNONLY_W			1
+#define FT_TCPFLAGS_W			12
 
 /* Some of the Compressed Filter Tuple fields have internal structure.  These
  * bit shifts/masks describe those structures.  All shifts are relative to the
